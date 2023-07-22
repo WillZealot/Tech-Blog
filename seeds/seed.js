c83ef12791f075ebaf394c1d6a2381ab21340a18
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-07-22 19:10:58 -0400

    time to test the seeds running into similar error i had before will try to fix

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,10 +1,16 @@
 const sequelize = require('../config/connection');
-const { User } = require('../models');
+const { User, Post } = require('../model');
 
 const userData = require('./userData.json');
+const userPosts = require('./UserPosts.json')
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
+
+  await Post.bulkCreate(userPosts, {
+    individualHooks: true,
+    returning: true,
+  });
 
   await User.bulkCreate(userData, {
     individualHooks: true,
