231ce2883019043aebd021754cac3e0b0c6c6bd3
william <williamtikhonenko@gmail.com>
2023-07-21 21:08:15 -0400

    added a index.js intot the controllers folder that will be the router file

--- controller/api/data.js
+++
@@ -1,2 +0,0 @@
-//add proper connections and routes here
-//remeber each route thats specific to one thing needs to be seperate
---
+++ controller/api/homepage.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+// const { } = require('../../models');
+
+// The `homepage endpoint`
+
+router.get('/', async (req, res) => {
+  try {
+    render.status(200).json(homeStuff);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//aye buddy get up in here and get it to route to the homepage aight
+//when this hits generate the homepage view
