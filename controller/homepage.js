29241b1bfe313e021554b6500beb403af8e7634c
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-07-25 18:34:56 -0400

    added a utils folder with a auth and a date fomrater inside

--- controller/homepage.js
+++ controller/homepage.js
@@ -5,12 +5,18 @@ const { Post } = require('../model');
 
 router.get('/', async (req, res) => {
   try {
-    const posts = await Post.findAll(req,res);
+    const postData = await Post.findAll({
+      order: [['title', 'ASC']],
+    });
+
+    // Serialize user data so templates can read it
+    const posts = postData.map((project) => project.get({ plain: true }));
+    // Pass serialized data into Handlebars.js template
     res.render('homepage', {posts});
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//aye buddy get up in here and get it to route to the homepage aight
-//when this hits generate the homepage view
+
+module.exports = router;
