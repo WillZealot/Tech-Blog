af2c91558270d582c6c1343511a6db480f84a43b
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-07-25 21:49:33 -0400

    edited my seeds and my login.js in the public folder

--- public/login.js
+++ public/login.js
@@ -1,51 +1,26 @@
-const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const email = document.querySelector('#email-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
-  
-    if (email && password) {
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
       const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ email, password }),
-        headers: { 'Content-Type': 'application/json' },
+          method: 'post',
+          body: JSON.stringify({
+              email,
+              password
+          }),
+          headers: {'Content-Type': 'application/json'}
       });
-  
       if (response.ok) {
-        document.location.replace('/');
+          document.location.replace('/dashboard');
       } else {
-        alert('Failed to log in.');
+          let result = await response.json()
+          alert(result.message)
       }
-    }
-  };
-  
-  const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (username && email && password) {
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ username, email, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to sign up.');
-      }
-    }
-  };
-  
-  document
-    .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
-  
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler);
+  }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
   
