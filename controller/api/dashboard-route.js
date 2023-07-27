3c761d0ecb773ef440585d448c36b9e7efd0dbb7
william <williamtikhonenko@gmail.com>
2023-07-27 12:38:21 -0400

    added dashboard route working on getting session id for curretn user

---
+++ controller/api/dashboard-route.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const { User } = require('../../model');
+
+router.get('/', async (req,res) => {
+    try {
+      const userData = await Session.findAll({
+      });
+
+      const loggedInUser = await User.findOne({ where: { id: userData.sid }, include: [{
+        model: 'post',
+        attribute: ''
+      }] });
+  
+      res.status(200).render('dashboard',{ loggedInUser});
+    } catch (err) {
+      res.status(400).json(err)
+    }
+  
+  
+  });
+
+
+module.exports = router;
--- controller/api/index.js
+++ controller/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
 const loginUserRoutes = require('./login-user');
+const dashboard = require('./dashboard-route')
 
 router.use('/user', userRoutes);
 router.use('/login', loginUserRoutes);
+router.use('/dashboard', dashboard)
 
 
 module.exports = router;
