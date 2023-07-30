81f7a0da963b0852c30f2770710c91d4aa80a28d
WillZealot <127908016+WillZealot@users.noreply.github.com>
2023-07-29 22:13:07 -0400

    for some reason logout isnt working as i hopped but login logout and dash work

--- controller/api/login-user.js
+++ controller/api/login-user.js
@@ -26,19 +26,25 @@ router.post('/', async (req, res) => {
     console.log(loggedUser)
 
     if (!dbUserData) {
-      res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+      res.status(400);
+      res.render('login');
+      alert('Incorrect email or password. Please try again!' );
+      
       return;
     }
 
     const validPassword = await dbUserData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+      res.status(400);
+      res.render('login');
+      alert('Incorrect email or password. Please try again!' );
       return;
     }
 
     req.session.save(() => {
       req.session.loggedIn = true;
+      req.session.userId = dbUserData.id;
       res.status(200).render('dashboard', { loggedUser, loggedIn: true });
     });
 
--- controller/dashboard-route.js
+++ controller/dashboard-route.js
@@ -1,23 +1,37 @@
 const router = require('express').Router();
-const { User } = require('../model');
+const { User, Post } = require('../model');
+const withAuth = require('../utils/auth'); // Import the custom middleware
 
-router.get('/', async (req,res) => {
-    try {
-      const userData = await Session.findAll({
-      });
+router.get('/', withAuth, async (req, res) => {
+  try {
+    // Fetch the user's data and their associated posts from the database using the req.session.userId
+    const dbUserData = await User.findOne({
+      where: {
+        id: req.session.userId, // Use the user ID from the session to find the user
+      },
+      include: {
+        model: Post,
+        attributes: ["title", "content", "date_created"]
+      }
+    });
 
-      const loggedInUser = await User.findOne({ where: { id: userData.sid }, include: [{
-        model: 'post',
-        attribute: ''
-      }] });
-  
-      res.status(200).render('dashboard',{ loggedInUser});
-    } catch (err) {
-      res.status(400).json(err)
+    if (!dbUserData) {
+      // If the user ID is not found in the database, handle the response accordingly (e.g., redirect to login page)
+      res.render('login'); // Redirect to the login page
+      return;
     }
-  
-  
-  });
 
+    // Convert the user data to a plain JavaScript object
+    const loggedUser = dbUserData.get({ plain: true });
+    console.log(loggedUser);
+
+    // Render the 'dashboard' template with the user's data and posts
+    res.status(200).render('dashboard', { loggedUser, loggedIn: true });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
